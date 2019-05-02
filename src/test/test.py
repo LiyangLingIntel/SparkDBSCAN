cfc0ebef14bde80ae226fa0d3eb59a7d6aa5346e
lyonling <935257580@qq.com>
2019-05-02 19:39:01 +0800

    efficiency test

--- src/test/test.py
+++ src/test/test.py
@@ -7,17 +7,17 @@ from src.utils import DataLoader, Evaluation
 from src.serial import NaiveDBSCAN, MatrixDBSCAN
 
 if __name__ == '__main__':
-    src = '../data/shape-sets/r15_600.txt'
+    src = '../data/shape-sets/pathbased_300.txt'
     dataset, _ = DataLoader.load_data_label(src)
 
-    print('Naive DBSCAN:')
-    ndbscan = NaiveDBSCAN(dataset, 0.7, 12)
-    ndbscan.predict()
-    del ndbscan
+    # print('Naive DBSCAN:')
+    # ndbscan = NaiveDBSCAN(dataset, 0.7, 12)
+    # ndbscan.predict()
+    # del ndbscan
 
     print('Matrix DBSCAN:')
-    eps = [0.7, ]
-    min_pts = [12, ]
+    eps = [0.6, 0.8, 1]
+    min_pts = [1, 2, 3]
     for i in eps:
         for j in min_pts:
             mdbscan = MatrixDBSCAN(dataset, i, j)
